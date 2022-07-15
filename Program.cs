ae2ff5c904a3277fafed506fda8bc6dcd595f838
Phil Bolduc <philbolduc@gmail.com>
2022-07-15 14:23:31 -0700

    Allow parameters from command line, remove async work in constructor, other perf and style changes

--- Program.cs
+++ Program.cs
@@ -1,27 +1,57 @@
 ﻿using Octokit;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.FileProviders.Physical;
+using System.Configuration;
 
 namespace bcgovneo4j
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
             Console.WriteLine("Started");
             var root = Directory.GetCurrentDirectory();
-            var dotenv = Path.Combine(root, ".env");
-            DotEnv.Load(dotenv);
+            var dotenv = ".env";
 
-            var config =
+            // default PhysicalFileProvider will hide DotPrefixed (.env) files, so disable exclusion
+            var fileProvider = new PhysicalFileProvider(root, ExclusionFilters.None);
+
+            // later configured sources override values from earlier configured sources
+            var configuration =
                 new ConfigurationBuilder()
+                .SetFileProvider(fileProvider)
+                .AddIniFile(dotenv, optional: true)
                 .AddEnvironmentVariables()
+                .AddCommandLine(args)
                 .Build();
 
-            var g = new GitData();
-            g.GetData().Wait();
+            try
+            {
+                var g = new GitData(configuration);
+                await g.GetData();
+                return 0;
+            }
+            catch (ConfigurationErrorsException exception)
+            {
+                OnError(exception, "Configuration Error", -1);
+            }
+            catch (AuthorizationException exception)
+            {
+                OnError(exception, "Github Token Error", -2);
+            }
+            catch (Exception exception)
+            {
+                OnError(exception, "Error", -2);
+            }
         }
 
+        private static int OnError(Exception exception, string label, int errorCode)
+        {
+            Console.Error.WriteLine();
+            Console.Error.WriteLine($"  {label}: {exception.Message}");
+            return errorCode;
 
-
+        }
     }
 }
--- Readme.md
+++ Readme.md
@@ -6,10 +6,26 @@ It is quick and dirty utilizing octokit for consuming the github api and then st
 
 ```docker run --publish=7474:7474 --publish=7687:7687 --volume=$HOME/neo4j/data:/data neo4j```
 
+## Configuration
+This application requires two configuration values:
+* GitHubToken
+* neo4jpassword
+
+The configuration can be supplied in various ways. The following list contains the configuration sources from highest to lowest priority. 
+Parameters with higher priority will override those with lower priority.
+1. Command line parameters
+1. Environment variables
+1. .env file - optional and must be in the current working directory
+
 Change .env_example to .env and then assign a GitHub PAT and update the password for you neo4j database.
 
 ```dotnet run```
 
+Command line examples
+
+```dotnet run --GitHubToken=<redacted> --neo4jpassword=<redacted>```
+
+```dotnet run /GitHubToken=<redacted> /neo4jpassword=<redacted>```
 
 # Cool Queries and examples
 
